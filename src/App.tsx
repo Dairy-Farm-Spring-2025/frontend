03b028c735671b2ad921eec0b4405dc012a757eb
namhuu6969 <namquangbinh6969@gmail.com>
2025-03-24 22:45:50 +0700

    connect firebase messaging for notification and config firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import { AnimatePresence } from 'framer-motion';
-import AppRouting from './config/routes/AppRouting';
 import { useNetworkStatus } from '@hooks/useNetworkStatus';
+import useToast from '@hooks/useToast';
+import { onMessage } from 'firebase/messaging';
+import { AnimatePresence } from 'framer-motion';
+import { useEffect } from 'react';
+import AppRouting from './config/routes/AppRouting';
+import { messaging, requestFCMToken } from './firebase';
 
 function App() {
+  const toast = useToast();
   useNetworkStatus();
+  useEffect(() => {
+    requestFCMToken();
+    onMessage(messaging, (payload) => {
+      console.log('Message received:', payload);
+      toast.showNotification(
+        `${payload.notification?.title}\n${payload.notification?.body}`
+      );
+    });
+  }, [toast]);
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+
+        // Gửi config vào Service Worker
+        if (registration.active) {
+          registration.active.postMessage({
+            type: 'SET_FIREBASE_CONFIG',
+            config: {
+              apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+              authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+              projectId: import.meta.env.VITE_PROJECT_ID,
+              storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+              messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER,
+              appId: import.meta.env.VITE_APP_ID,
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
 
   return (
     <AnimatePresence mode="wait">
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
+import '@xyflow/react/dist/style.css';
 import { ConfigProvider, Spin } from 'antd';
+import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
+import { SiHappycow } from 'react-icons/si';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
+import './config/i18n';
 import { persistor, store } from './core/store/store.ts';
 import './index.scss';
-import './config/i18n';
-import { Suspense } from 'react';
-import { SiHappycow } from 'react-icons/si';
-import '@xyflow/react/dist/style.css';
 
 createRoot(document.getElementById('root')!).render(
   <>
@@ -26,8 +26,7 @@ createRoot(document.getElementById('root')!).render(
           Menu: {
             itemSelectedBg: 'rgba(22, 101, 52, 0.1)',
           },
-          Input: {
-          },
+          Input: {},
         },
       }}
     >
